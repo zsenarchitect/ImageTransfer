216e4bca1dae308acea7511974914c90fd0c5ecc
zsenarchitect <zsenarchitect@gmail.com>
2023-07-04 11:05:20 -0400

    update GUI

--- Projects/ImageTransferApp/models.py
+++ Projects/ImageTransferApp/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, blank=True)
--- Projects/ImageTransferApp/views.py
+++ Projects/ImageTransferApp/views.py
@@ -1,13 +1,21 @@
-from email.mime import image
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Image
+from .forms import ImageForm
+
 
 def image_upload(request):
     if request.method == 'POST':
-        image = Image(image=request.FILES['image'])
-        image.save()
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_image = form.save(commit=False)  # Don't save the model yet
+            new_image.name = request.FILES['image'].name  # Get the name of the file
+            new_image.save()  # Now save the model
+            return redirect('all_images')  # Or 'all' if you want to redirect to the all images page
+    else:
+        form = ImageForm()
 
-    return render(request, 'upload.html')
+    return render(request, 'upload.html', {'form': form})
+
 
 def image_view(request):
     images = Image.objects.all()
