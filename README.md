f68b1a9a963e4ee5e78075267dedcfa5fad08af3
zsenarchitect <zsenarchitect@gmail.com>
2023-07-05 08:08:32 -0400

    update main

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ImageTransfer
+
+
+
+
+to reset database
+python manage.py flush
+
+
+to run server
+python manage.py runserver
+
+
+to migrat
+python manage.py makemigrations
+python manage.py migrate
--- external_scripts/download_all_images.py
+++ external_scripts/download_all_images.py
@@ -16,6 +16,14 @@ response.raise_for_status()
 # Get the JSON data from the response
 image_data = response.json()
 
+# make sure output folder exists
+output_folder = os.path.join(os.path.expanduser('~'), 'Desktop', "DB_images_preview")
+os.makedirs(output_folder, exist_ok=True)
+
+# delete all the files in this output folder
+for filename in os.listdir(output_folder):
+    os.remove(os.path.join(output_folder, filename))
+
 # Iterate over the image data and download each image
 for image in image_data:
     # Send a GET request to the image URL
@@ -23,9 +31,11 @@ for image in image_data:
     response.raise_for_status()
     
     # Create the path for the image file
-    filename = os.path.join(os.path.expanduser('~'), 'Desktop', "DB_" + image['name'])
+    filename = os.path.join(output_folder, "DB_" + image['name'])
     
     # Write the image data to a file
     with open(filename, 'wb') as f:
         f.write(response.content)
         print (filename)
+
+os.startfile(output_folder)
